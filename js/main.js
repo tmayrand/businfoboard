2c3b98b16e984728d18ce9e5a6abeccd885081bb
T. Mayrand <tmayrand@users.noreply.github.com>
2018-11-25 14:09:42 -0500

    fixed edits

--- js/main.js
+++ js/main.js
@@ -25,9 +25,9 @@ var options =
 {
   url: "https://bustracker.pvta.com/InfoPoint/rest/",
   //title: "North Pleasant Street - Departures",  // title to display at top of page
-  title: "Orchard Hill Residential
+  title: "Orchard Hill Residential",
   //stops: ["58","56","64","63","71","72"], // Stops on North Pleasant
-  stops: ["59","62"]
+  stops: ["59","62"],
   routes: ["30", "31", "33", "34", "35", "36", "45", "46"],
   excluded_trips: [],
   sort: "route",                  // default way to sort departures
